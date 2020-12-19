8e4c79b69a238107e19cfce251e4bc1c00fe0e0d
zhenhua-cai <steven.cai1990@hotmail.com>
2020-12-18 18:29:46 -0600

    remove history

--- stevenblog-version-1.0/back-end/out/artifacts/stevenblog_version1_war_exploded/WEB-INF/resources/js/aboutme.js
+++
@@ -1,6 +0,0 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    // End of use strict
---
+++ stevenblog-version-2.0/backend/src/test/java/net/stevencai/blog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.stevencai.blog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
