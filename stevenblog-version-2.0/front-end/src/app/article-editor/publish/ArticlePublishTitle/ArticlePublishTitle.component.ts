97e07716e67b2018b5672d8789713739594797e1
zhenhua-cai <steven.cai1990@hotmail.com>
2021-01-05 13:32:53 -0600

    fixed some visual issues

--- stevenblog-version-2.0/front-end/src/app/article-editor/publish/ArticlePublishTitle/ArticlePublishTitle.component.ts
+++ stevenblog-version-2.0/front-end/src/app/article-editor/publish/ArticlePublishTitle/ArticlePublishTitle.component.ts
@@ -53,7 +53,7 @@ export class ArticlePublishTitleComponent implements OnInit, OnDestroy {
       return;
     }
     this.publishService.updatePublishArticle(this.article);
-    this.router.navigate(['/account/publish/sign-up-confirmation']);
+    this.router.navigate(['/account/publish/confirmation']);
     return;
 
   }
